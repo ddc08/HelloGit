7d1b359156019463bd15e85051c39428a90cef2b
ddc08 <1174874390@qq.com>
2020-09-01 09:14:40 +0800

    test

---
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author DDC
+ * @date 2020/9/1- 8:59
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("HelloGit!");
+    }
+}
